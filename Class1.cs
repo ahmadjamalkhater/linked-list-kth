30b85e661d5b7b54378785703d568345131e3dda
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-07-03 07:15:06 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linked_list_kth
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public int KthFromEnd(int k)
+        {
+            if (Head == null)
+            {
+                throw new Exception("The linked list is empty.");
+            }
+
+            if (k < 0)
+            {
+                throw new Exception("Invalid value for k.");
+            }
+
+            Node slowPtr = Head;
+            Node fastPtr = Head;
+
+            // Move the fast pointer k nodes ahead of the slow pointer
+            for (int i = 0; i < k; i++)
+            {
+                if (fastPtr.Next != null)
+                {
+                    fastPtr = fastPtr.Next;
+                }
+                else
+                {
+                    throw new Exception("The linked list does not have k nodes.");
+                }
+            }
+
+            // Move both pointers until the fast pointer reaches the end
+            while (fastPtr.Next != null)
+            {
+                slowPtr = slowPtr.Next;
+                fastPtr = fastPtr.Next;
+            }
+
+            return slowPtr.Value;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace linked_list_kth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList ll = new LinkedList();
+            ll.Head = new Node(1);
+            ll.Head.Next = new Node(3);
+            ll.Head.Next.Next = new Node(8);
+            ll.Head.Next.Next.Next = new Node(2);
+
+            Console.WriteLine(ll.KthFromEnd(0));  // Output: 2
+            Console.WriteLine(ll.KthFromEnd(2));  // Output: 3
+
+        }
+    }
+}
